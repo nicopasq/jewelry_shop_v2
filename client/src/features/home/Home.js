627b18e083a3a7c4b7c82356ea70e693f53605c0
Nico <nicopasquariello@gmail.com>
2023-11-24 14:56:35 -0700

    Featured products scroll does not exceed array length.

--- client/src/features/home/Home.js
+++ client/src/features/home/Home.js
@@ -7,6 +7,10 @@ import ring_images from "../../images/rings/rings.js";
 
 function Home(){
     const [products, setProducts] = useState([])
+    const [sliceVal, setSliceVal] = useState({
+        start: 0,
+        end: 3
+    })
 
 useEffect(() => {
     fetch('/products')
@@ -42,29 +46,63 @@ const featuredProducts = testArr.map(product => {
         </Grid>
     )
 })
-const [sliceVal, setSliceVal] = useState({
-    start: 0,
-    end: 3
-})
 const renderFeatured = featuredProducts.slice(sliceVal.start, sliceVal.end)
 console.log(renderFeatured)
 const lastBtn = '<'
 const nextBtn = '>'
+
+
+function scroll(e){
+    // const max = 12
+    if (e.target.name ==="backBtn"){
+        let start = sliceVal.start-3
+        if (start < 0){
+            setSliceVal({start: 0, end: 3})
+        } else if (start >= 0 ){
+            setSliceVal({start: start, end: start + 3})
+        }
+    } else if (e.target.name === "nextBtn"){
+        let start = sliceVal.start+3
+        if (start >= 9){
+            setSliceVal({start: 9, end: 12})
+        } else if (start <= 9 ){
+            setSliceVal({start: start, end: start + 3})
+        }
+    }
+}
+console.log(sliceVal)
     return (
     <>
         <Navbar/>
         <Container sx={{border:'1px solid black'}}>
             <img src={homeCover} id="homeCover"/>
             <Button variant="outlined" id="homeCoverBtn">Shop Now!</Button>
+            
             <>
                 <Typography variant="h3">Featured Products</Typography>
-            <div id="featured" className="shell" style={{border:'1px solid black', height:'40vh', width:'60vw'}}>
+            <div id="featured" className="shell">
                 <div id="featuredProductsContainer" className="productContainer">
-                    <Button className="backBtn" variant="contained" onClick={() => setSliceVal({start: sliceVal.start-3, end: sliceVal.end-3})} style={{position:'relative', right:'28vw', top:'13vh', height:'15vh'}}>{lastBtn}</Button>
+                    
+                    <Button 
+                    className="backBtn" 
+                    name= 'backBtn'
+                    variant="contained" 
+                    onClick={(e) => scroll(e)}>
+                        {lastBtn}
+                    </Button>
+
                     <div id="featuredProducts" className="productDisplay">
                         {renderFeatured}
                     </div>
-                    <Button className="nextBtn" variant="contained" onClick={() => setSliceVal({start: sliceVal.start+3, end: sliceVal.end+3})} style={{position:'relative', left:'28vw', bottom:'2vh', height:'15vh'}}>{nextBtn}</Button>
+
+                    <Button 
+                    className="nextBtn" 
+                    name= 'nextBtn'
+                    variant="contained" 
+                    onClick={(e) =>scroll(e)}>
+                        {nextBtn}
+                    </Button>
+
                 </div>
             </div>
             </>
