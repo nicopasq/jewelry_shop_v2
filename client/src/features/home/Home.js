d124aeecd88c35de9315ce811f7a052cdfe94ecc
Nico <nicopasquariello@gmail.com>
2023-11-24 14:31:56 -0700

    Added a Featured Products section on home page, buttons will 'scroll' through products.

--- client/src/features/home/Home.js
+++ client/src/features/home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../navigation/Navbar"; 
 import '../../styles/home.css'
-import { Button, Container } from "@mui/material";
+import { Button, Card, CardContent, CardHeader, Container, Grid, Typography } from "@mui/material";
 import homeCover from '../home/rings_home_cover.jpg'
 import ring_images from "../../images/rings/rings.js";
 
@@ -11,29 +11,64 @@ function Home(){
 useEffect(() => {
     fetch('/products')
     .then(r => r.json())
-    .then(data => setProducts(data))
+    .then(data => {
+        console.log('data', data)
+        data.map(product => {
+            ring_images.map(r => {
+                if (r.includes(product.image_path)){
+                    product.image = r
+                }
+            })
+        })
+        setProducts(data)
+    })
 },[])
 
-const addImage = products.map(p => {
-    ring_images.map(r => {
-        if (r.includes(p.image_path)){
-            p.image = r
-            console.log(p)
-        }
-    })
-    return (
-        <img src={p.image}/>
+
+const testArr = [1,2,3,4,5,6,7,8,9,10,11,12]
+const featuredProducts = testArr.map(product => {
+    return ( 
+        <Grid item xs={4}>
+        <Card>
+            {/* <img src={ring.image} key={ring.id} style={{height:'25vh', margin:'5px', border:'1px solid black'}}/> */}
+            {product}
+            <CardContent>
+                {/* <Typography>{ring.product_name}</Typography>
+                <Typography>{ring.price}</Typography> */}
+                <Typography>ring name</Typography>
+                <Typography>price</Typography>
+            </CardContent>
+        </Card>
+        </Grid>
     )
 })
-console.log(addImage)
-
+const [sliceVal, setSliceVal] = useState({
+    start: 0,
+    end: 3
+})
+const renderFeatured = featuredProducts.slice(sliceVal.start, sliceVal.end)
+console.log(renderFeatured)
+const lastBtn = '<'
+const nextBtn = '>'
     return (
     <>
         <Navbar/>
-        <Container>
+        <Container sx={{border:'1px solid black'}}>
             <img src={homeCover} id="homeCover"/>
             <Button variant="outlined" id="homeCoverBtn">Shop Now!</Button>
-            {addImage}
+            <>
+                <Typography variant="h3">Featured Products</Typography>
+            <div id="featured" className="shell" style={{border:'1px solid black', height:'40vh', width:'60vw'}}>
+                <div id="featuredProductsContainer" className="productContainer">
+                    <Button className="backBtn" variant="contained" onClick={() => setSliceVal({start: sliceVal.start-3, end: sliceVal.end-3})} style={{position:'relative', right:'28vw', top:'13vh', height:'15vh'}}>{lastBtn}</Button>
+                    <div id="featuredProducts" className="productDisplay">
+                        {renderFeatured}
+                    </div>
+                    <Button className="nextBtn" variant="contained" onClick={() => setSliceVal({start: sliceVal.start+3, end: sliceVal.end+3})} style={{position:'relative', left:'28vw', bottom:'2vh', height:'15vh'}}>{nextBtn}</Button>
+                </div>
+            </div>
+            </>
+            <br></br>
         </Container>
     </>
     )
