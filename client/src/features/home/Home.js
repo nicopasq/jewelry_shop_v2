b639367d8dc9de8f16fd529df4dab714c67456f7
Nico <nicopasquariello@gmail.com>
2023-11-24 15:08:27 -0700

    Created ProductScroll.js component.

--- client/src/features/home/Home.js
+++ client/src/features/home/Home.js
@@ -4,13 +4,10 @@ import '../../styles/home.css'
 import { Button, Card, CardContent, CardHeader, Container, Grid, Typography } from "@mui/material";
 import homeCover from '../home/rings_home_cover.jpg'
 import ring_images from "../../images/rings/rings.js";
+import ProductScroll from "./ProductScroll.js";
 
 function Home(){
     const [products, setProducts] = useState([])
-    const [sliceVal, setSliceVal] = useState({
-        start: 0,
-        end: 3
-    })
 
 useEffect(() => {
     fetch('/products')
@@ -28,84 +25,18 @@ useEffect(() => {
     })
 },[])
 
-
-const testArr = [1,2,3,4,5,6,7,8,9,10,11,12]
-const featuredProducts = testArr.map(product => {
-    return ( 
-        <Grid item xs={4}>
-        <Card>
-            {/* <img src={ring.image} key={ring.id} style={{height:'25vh', margin:'5px', border:'1px solid black'}}/> */}
-            {product}
-            <CardContent>
-                {/* <Typography>{ring.product_name}</Typography>
-                <Typography>{ring.price}</Typography> */}
-                <Typography>ring name</Typography>
-                <Typography>price</Typography>
-            </CardContent>
-        </Card>
-        </Grid>
-    )
-})
-const renderFeatured = featuredProducts.slice(sliceVal.start, sliceVal.end)
-console.log(renderFeatured)
-const lastBtn = '<'
-const nextBtn = '>'
-
-
-function scroll(e){
-    // const max = 12
-    if (e.target.name ==="backBtn"){
-        let start = sliceVal.start-3
-        if (start < 0){
-            setSliceVal({start: 0, end: 3})
-        } else if (start >= 0 ){
-            setSliceVal({start: start, end: start + 3})
-        }
-    } else if (e.target.name === "nextBtn"){
-        let start = sliceVal.start+3
-        if (start >= 9){
-            setSliceVal({start: 9, end: 12})
-        } else if (start <= 9 ){
-            setSliceVal({start: start, end: start + 3})
-        }
-    }
-}
-console.log(sliceVal)
     return (
     <>
         <Navbar/>
         <Container sx={{border:'1px solid black'}}>
             <img src={homeCover} id="homeCover"/>
             <Button variant="outlined" id="homeCoverBtn">Shop Now!</Button>
-            
-            <>
-                <Typography variant="h3">Featured Products</Typography>
-            <div id="featured" className="shell">
-                <div id="featuredProductsContainer" className="productContainer">
-                    
-                    <Button 
-                    className="backBtn" 
-                    name= 'backBtn'
-                    variant="contained" 
-                    onClick={(e) => scroll(e)}>
-                        {lastBtn}
-                    </Button>
-
-                    <div id="featuredProducts" className="productDisplay">
-                        {renderFeatured}
-                    </div>
-
-                    <Button 
-                    className="nextBtn" 
-                    name= 'nextBtn'
-                    variant="contained" 
-                    onClick={(e) =>scroll(e)}>
-                        {nextBtn}
-                    </Button>
-
-                </div>
-            </div>
-            </>
+            <ProductScroll containerInfo={
+                {
+                    name:'Featured Products',
+                    productArr:[1,2,3,4,5,6,7,8,9,10,11,12]
+                }
+            }/>             
             <br></br>
         </Container>
     </>
---
+++ client/src/features/home/ProductScroll.js
@@ -0,0 +1,79 @@
+import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
+import React, { useState } from "react";
+
+function ProductScroll({ containerInfo }) {
+  const [sliceVal, setSliceVal] = useState({
+    start: 0,
+    end: 3,
+  });
+
+  const products = containerInfo.productArr.map((product) => {
+    return (
+      <Grid item xs={4}>
+        <Card>
+          {/* <img src={ring.image} key={ring.id} style={{height:'25vh', margin:'5px', border:'1px solid black'}}/> */}
+          {product}
+          <CardContent>
+            {/* <Typography>{ring.product_name}</Typography>
+                    <Typography>{ring.price}</Typography> */}
+            <Typography>ring name</Typography>
+            <Typography>price</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  });
+  const renderProducts = products.slice(sliceVal.start, sliceVal.end);
+
+  const lastBtn = "<";
+  const nextBtn = ">";
+
+  function scroll(e) {
+    // const max = 12
+    if (e.target.name === "backBtn") {
+      let start = sliceVal.start - 3;
+      if (start < 0) {
+        setSliceVal({ start: 0, end: 3 });
+      } else if (start >= 0) {
+        setSliceVal({ start: start, end: start + 3 });
+      }
+    } else if (e.target.name === "nextBtn") {
+      let start = sliceVal.start + 3;
+      if (start >= 9) {
+        setSliceVal({ start: 9, end: 12 });
+      } else if (start <= 9) {
+        setSliceVal({ start: start, end: start + 3 });
+      }
+    }
+  }
+  return (
+    <>
+      <Typography variant="h3">{containerInfo.name}</Typography>
+      <div className="shell">
+        <div className="productContainer">
+          <Button
+            className="backBtn"
+            name="backBtn"
+            variant="contained"
+            onClick={(e) => scroll(e)}
+          >
+            {lastBtn}
+          </Button>
+
+          <div className="productDisplay">{renderProducts}</div>
+
+          <Button
+            className="nextBtn"
+            name="nextBtn"
+            variant="contained"
+            onClick={(e) => scroll(e)}
+          >
+            {nextBtn}
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductScroll;
