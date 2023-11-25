2fc48917ca011b6c53ebaa546dcbd413314e5443
Nico <nicopasquariello@gmail.com>
2023-11-25 13:48:52 -0700

    added seed data for rings and put all images into ring_images.

--- client/src/images/rings/rings.js
+++ client/src/images/rings/rings.js
@@ -1,7 +1,32 @@
 import blackOpal from '../rings/black_opal.jpg'
 import twoGoldBand from '../rings/gold_band_set_of_2.jpg'
 import goldPeridot from '../rings/gold_bezel_peridot.jpg'
+import goldDiamondOpal from '../rings/gold_diamond_opal.jpg'
+import goldDiamondAlexandrite from '../rings/gold_diamonds&_alexandrite.jpg'
+import goldHexagonAlexandrite from '../rings/gold_hexagon_alexandrite.jpg'
+import goldLeafRuby from '../rings/gold_leaf_ruby.jpg'
+import goldSapphireEmerald from '../rings/gold_plated_saphire_emerald.jpg'
+import goldRoundAlexandrite from '../rings/gold_round_alexandrite.jpg'
+import goldRuby from '../rings/gold_ruby.jpg'
+import roseGoldDiamondMossAgate from '../rings/rose_gold_diamond&_mossAgate.jpg'
+import roseGoldKiteMossAgate from '../rings/rose_gold_kite_mossAgate.png'
+import sterlingAustrailianOpal from '../rings/sterling_australian_opal.jpg'
+import twoSterlingBands from '../rings/sterling_band_set_of_2.jpg'
+import sterlingDiamond from '../rings/sterling_diamond.jpg'
+import sterlingDiamondSapphire from '../rings/sterling_diamond&_sapphire_band.jpg'
+import sterlingFlowerEngraving from '../rings/sterling_flower_engraving.jpg'
+import sterlingLeafMossAgate from '../rings/sterling_leaf_mossAgate.jpg'
+import sterlingDiamondSapphireInlay from '../rings/sterling_saphire&_diamond_inlay.jpg'
+import sterlingStarDiamond from '../rings/sterling_star_diamond.jpg'
+import sterlingWaveEngraving from '../rings/sterling_wave_engraving.jpg'
+import turtleSetOfTwo from '../rings/turtle_set_of_2.jpg'
+import whiteGoldDiamondRuby3P from '../rings/white_gold_3piece_diamond&_ruby.jpg'
+import whiteGoldDiamondTealGreenSapphire from '../rings/white_gold_diamond_tealGreen_sapphire.jpg'
+import whiteGoldFireOpal from '../rings/white_gold_fire_opal.jpg'
+import whiteGoldProngEmerald from '../rings/white_gold_prong_emerald.jpg'
 
-const ring_images = [blackOpal, twoGoldBand, goldPeridot]
+
+
+const ring_images = [whiteGoldProngEmerald,whiteGoldFireOpal,whiteGoldDiamondTealGreenSapphire,whiteGoldDiamondRuby3P,turtleSetOfTwo,sterlingWaveEngraving,sterlingStarDiamond,sterlingDiamondSapphireInlay,sterlingLeafMossAgate,sterlingFlowerEngraving,sterlingDiamondSapphire,sterlingDiamond,twoSterlingBands,sterlingAustrailianOpal,roseGoldKiteMossAgate,roseGoldDiamondMossAgate,goldRuby, goldRoundAlexandrite, blackOpal, twoGoldBand, goldPeridot, goldDiamondOpal, goldDiamondAlexandrite, goldHexagonAlexandrite, goldLeafRuby, goldSapphireEmerald]
 
 export default ring_images
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create(product_name:'Black Opal', image_path:'/black_opal', product_type:'ring', price:250 ,in_stock: true)
+
+Product.create(product_name:'twoGoldBand', image_path:'/gold_band_set_of_2', product_type:'ring', price:100, in_stock: true)
+
+Product.create(product_name:'goldPeridot', image_path:'/gold_bezel_peridot', product_type:'ring', price:150, in_stock: true)
+
+Product.create(product_name:'goldDiamondOpal', image_path:'/gold_diamond_opal', product_type:'ring', price:275, in_stock: true)
+
+Product.create(product_name:'goldDiamondAlexandrite', image_path:'/gold_diamond_alexandrite', product_type:'ring', price:200, in_stock: true)
+
+Product.create(product_name:'goldHexagonAlexandrite', image_path:'/gold_hexagon_alexandrite', product_type:'ring', price:220, in_stock: true)
+
+Product.create(product_name:'goldLeafRuby', image_path:'/gold_leaf_ruby', product_type:'ring', price:300, in_stock: true)
+
+Product.create(product_name:'goldSapphireEmerald', image_path:'/gold_sapphire_emerald', product_type:'ring', price:233, in_stock: true)
+
+Product.create(product_name:'goldRoundAlexandrite', image_path:'/gold_round_alexandrite', product_type:'ring', price:175, in_stock: true)
+
+Product.create(product_name:'goldRuby', image_path:'/gold_ruby', product_type:'ring', price:275, in_stock: true)
+
+Product.create(product_name:'roseGoldDiamondMossAgate', image_path:'/rose_gold_diamond_mossAgate', product_type:'ring', price:350, in_stock: true)
+
+Product.create(product_name:'roseGoldKiteMossAgate', image_path:'/rose_gold_kite_mossAgate', product_type:'ring', price:320, in_stock: true)
+
+Product.create(product_name:'sterlingAustrailianOpal', image_path:'/sterling_austrailian_opal', product_type:'ring', price:200, in_stock: true)
+
+Product.create(product_name:'twoSterlingBands', image_path:'/sterling_band_set_of_2', product_type:'ring', price:100, in_stock: true)
+
+Product.create(product_name:'sterlingDiamond', image_path:'/sterling_diamond', product_type:'ring', price:150, in_stock: true)
+
+Product.create(product_name:'sterlingDiamondSapphire', image_path:'/sterling_diamond_sapphire', product_type:'ring', price:235, in_stock: true)
+
+Product.create(product_name:'sterlingFlowerEngraving', image_path:'/sterling_flower_engraving', product_type:'ring', price:70, in_stock: true)
+
+Product.create(product_name:'sterlingLeafMossAgate', image_path:'/sterling_leaf_mossAgate', product_type:'ring', price:143, in_stock: true)
+
+Product.create(product_name:'sterlingDiamondSapphireInlay', image_path:'/sterling_diamond_sapphire_inlay', product_type:'ring', price:175, in_stock: true)
+
+Product.create(product_name:'sterlingStarDiamond', image_path:'/sterling_star_diamond', product_type:'ring', price:150, in_stock: true)
+
+Product.create(product_name:'sterlingWaveEngraving', image_path:'/sterling_wave_engraving', product_type:'ring', price:50, in_stock: true)
+
+Product.create(product_name:'turtleSetOfTwo', image_path:'/turtle_set_of_2', product_type:'ring', price:50, in_stock: true)
+
+Product.create(product_name:'whiteGoldDiamondRuby3P', image_path:'/white_gold_3piece_diamond&_ruby', product_type:'ring', price:175, in_stock: true)
+
+Product.create(product_name:'whiteGoldDiamondTealGreenSapphire', image_path:'/white_gold_diamond_teal_green_sapphire', product_type:'ring', price:247, in_stock: true)
+
+Product.create(product_name:'whiteGoldFireOpal', image_path:'/white_gold_fire_opal', product_type:'ring', price:300, in_stock: true)
+
+Product.create(product_name:'whiteGoldProngEmerald', image_path:'/white_gold_prong_emerald', product_type:'ring', price:300, in_stock: true)
