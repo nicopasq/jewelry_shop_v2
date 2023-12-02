6539a8fa68368c0cb5be5182eab294e18c3e046e
Nico <nicopasquariello@gmail.com>
2023-12-01 20:07:31 -0700

    Added profile page content

--- client/src/features/profile/Profile.js
+++ client/src/features/profile/Profile.js
@@ -1,9 +1,46 @@
 import React from "react";
+import Navbar from "../navigation/Navbar";
+import { Card, Container, Paper, Table, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import '../../styles/profile.css'
 
 function Profile(){
+    const currentUser = useSelector(state => state.currentUser.value)
+    const dateTime = currentUser.created_at.split('T')
+    const date = dateTime[0].split('-')
+    const [year, month, day] = date
+    date[0] = month
+    date[1] = day
+    date[2] = year
+    const joinDate = date.join('-')
 return(
     <div>
-        <h1>Profile</h1>
+        <Navbar/>
+
+        <Container>
+            <Card elevation={10} id="profileCard">
+                <Typography variant="h4" className="profileCardData"><u>{currentUser.username}</u></Typography>
+                <br/>
+                <Typography variant="h5" className="profileCardData">Joined:{joinDate}</Typography>
+                <br/>
+                <br/>
+                <Typography variant="h5" className="profileCardData">Total Orders:</Typography>
+            </Card>
+
+            <Paper elevation={6} className="dataContainer">
+                <div className="containerHeader">
+                    <Typography variant="body1" className="containerTitle" sx={{fontFamily:'monospace'}}>Items Ordered</Typography>
+                </div>
+                <Table id='itemsTable'></Table>
+            </Paper>
+
+            <Paper elevation={6} className="dataContainer">
+                <div className="containerHeader">
+                    <Typography variant="body1" className="containerTitle" sx={{fontFamily:'monospace'}}>All Orders</Typography>
+                </div>  
+                <Table id='ordersTable'></Table>
+            </Paper>
+        </Container>
     </div>
 )
 }
