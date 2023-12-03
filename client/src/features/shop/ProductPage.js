043a9cb7c64d96410a20093bd882d27d28380d74
Nico <nicopasquariello@gmail.com>
2023-12-03 16:58:46 -0700

    product page details are styled

--- client/src/features/shop/ProductPage.js
+++ client/src/features/shop/ProductPage.js
@@ -6,11 +6,11 @@ import '../../styles/productPage.css'
 import { Divider, InputLabel, MenuItem, Select, Typography } from "@mui/material";
 
 function ProductPage(){
-    const [ringSize, setRingSize] = useState([])
+    const [ringSize, setRingSize] = useState('')
     const [currentProduct, setCurrentProduct] = useState({})
     const { id } = useParams()
     const ringSizes = [5.0, 5.25, 5.5, 6.0, 6.25, 6.5, 7.0, 7.25, 7.5, 8.0, 8.25, 8.5, 9.0, 9.25, 9.5, 10, 10.25, 10.5, 11]
-    const menuItems = ringSizes.map(size => <MenuItem value={size}>{size}</MenuItem>)
+    const menuItems = ringSizes.map(size => <MenuItem value={size} key={size}>{size}</MenuItem>)
 
     useEffect(() => {
         fetch(`/products/${id}`)
@@ -32,31 +32,34 @@ function ProductPage(){
                 <img src={currentProduct.image} alt={currentProduct.product_name} id="productImage"/>
             </div>
             <div id="productDetails" className="halfScreen">
-                <div id="productDetails">
-                    <Typography variant="h3">{currentProduct.product_name}</Typography>
-                    <Typography variant="h4"><b>Free Shipping & Free 90 Day Returns</b></Typography>
-                    <Divider/>
+                
+                <div id="productData">
+                    <Typography variant="h3" id="productName" className="productData">{currentProduct.product_name}</Typography>
+                    <Divider sx={{bgcolor:'lightGrey'}}/>
                 </div>
                 
                 <div id="ringSize">
-                    <form>
+                    <form id="sizeForm" className="productData">
                         <InputLabel id="ringSizes">Ring Size:</InputLabel>
                         <Select 
                             labelId="ringSizes"
                             id="selectSize"
                             value={ringSize}
-                            label='Ring Size'
+                            label='RingSize'
+                            placeholder="Select Ring Size"
+                            sx={{height:'6vh', width:'15vw', position:'relative', bottom:"5vh"}}
                             onChange={(e) => setRingSize(e.target.value)}>
                                 {menuItems}
                             </Select>
                     </form>
-                    <Divider/>
+                    <Divider sx={{bgcolor:'lightGrey'}}/>
                 </div>
 
                 <div id="payment">
-                    <Typography variant="h5">Total</Typography>
-                    <Divider/>
-                    <Typography variant="h4">${currentProduct.price}</Typography>
+                    <Typography variant="h5" className="productData" id="total" >Total:</Typography>
+                    <Divider sx={{bgcolor:'lightGrey'}} />
+                    <Typography variant="h4" className="productData">${currentProduct.price}</Typography>
+                    <Typography variant="h4" className="productData"><b>Free Shipping & Free 90 Day Returns</b></Typography>
                 </div>
             </div>
         </div>
