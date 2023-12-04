f80ebe0ddf810d2f79bc10052d890ed6000fdea7
Nico <nicopasquariello@gmail.com>
2023-12-03 21:01:44 -0700

    Products are added to cart per user.

--- client/src/features/bag/Bag.js
+++ client/src/features/bag/Bag.js
@@ -1,17 +1,41 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Card, CardContent, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../navigation/Navbar';
 import { useSelector } from 'react-redux';
 
 function Bag(){
     const currentUser = useSelector(state => state.currentUser.value)
-    const saveForLater = currentUser.order_products.filter(order => order.in_cart === false)
-    const inBag = currentUser.order_products.filter(order => order.in_cart === true)
-console.log('inBag', inBag)
+    const products = useSelector(state => state.products.value)
+    const orderProducts = []    
+    currentUser.order_products.map(orderProduct => {
+        products?.map(p => {
+            if(p.id === orderProduct.product_id){
+                orderProducts.push({...orderProduct, image: p.image})
+            }
+        })
+    })
+    const inBag = orderProducts.filter(p => p.in_cart === true)
+    const saveForLate = orderProducts.filter(p => p.in_cart === false)
+
+    console.log(inBag)
+
+    const tempDisplay = inBag.map(product => {
+        const productObj = product.product
+        return (
+            <div key={product.id}>
+                <Card>
+                    <img src={product.image} style={{height:'20vh'}}/>
+                    <CardContent>
+                        <Typography variant='h6'>{productObj.product_name}</Typography>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    })
     return(
         <div className='main'>
             <Navbar/>
-            <div></div>
+            {tempDisplay}
         </div>
     )
 }
