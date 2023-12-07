ea5f0b71e39b7d4ecc76cbcebdedcdc16927b771
Nico Pasquariello <nicopasquariello@gmail.com>
2023-12-07 12:26:34 -0700

    begining to create bag page, attempted to fix styling issues for laptop display.

--- client/src/features/bag/Bag.js
+++ client/src/features/bag/Bag.js
@@ -1,7 +1,9 @@
-import { Card, CardContent, Typography } from '@mui/material'
+import { Button, Card, CardActionArea, CardContent, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import Navbar from '../navigation/Navbar';
 import { useSelector } from 'react-redux';
+import '../../styles/bag.css'
+import { Link } from 'react-router-dom';
 
 function Bag(){
     const currentUser = useSelector(state => state.currentUser.value)
@@ -15,27 +17,45 @@ function Bag(){
         })
     })
     const inBag = orderProducts.filter(p => p.in_cart === true)
-    const saveForLate = orderProducts.filter(p => p.in_cart === false)
+    const saveForLater = orderProducts.filter(p => p.in_cart === false)
 
-    console.log(inBag)
-
-    const tempDisplay = inBag.map(product => {
+    //make into bagDisplay component
+    const bagDisplay = inBag.map(product => {
         const productObj = product.product
         return (
-            <div key={product.id}>
-                <Card>
-                    <img src={product.image} style={{height:'20vh'}}/>
-                    <CardContent>
+                <Card key={product.id} className='productCardBag'>
+                    <img src={product.image} />
+                    <CardContent className='cardContent'>
                         <Typography variant='h6'>{productObj.product_name}</Typography>
+                        <Typography variant='h6'>${productObj.price}</Typography>
+                        <Typography variant='h6'>Quantity: {product.quantity}</Typography>
+                        <Typography variant='h6'>Size: {product.size}</Typography>
+                    </CardContent>
+                    <CardContent className='cardButtons' sx={{width:'30%'}}>
+                        <Button onClick={() => deleteOrderProduct(product)}>Remove Item</Button> 
+                        <Button>Save For Later</Button>
                     </CardContent>
                 </Card>
-            </div>
         )
     })
+    const nothingInBag = (
+        <Link to='/shop/'>
+            <Typography variant='h6' sx={{fontFamily:'serif'}}>
+                Continue shopping
+            </Typography>
+        </Link>)
+
+    const render = bagDisplay.length > 0 ? bagDisplay : nothingInBag;
     return(
         <div className='main'>
             <Navbar/>
-            {tempDisplay}
+            <div id='totalContainer'>
+
+            </div>
+            <div id='bag'>
+            <Typography variant='h5' sx={{fontFamily:"serif"}}>Your Bag</Typography>
+                {render}
+            </div>
         </div>
     )
 }
