a6096eecb04001e4bdb13218aad070e7ad013b0d
Nico Pasquariello <nicopasquariello@gmail.com>
2023-12-13 16:33:32 -0700

    added stepper for order information.

--- client/src/features/checkout/Checkout.js
+++ client/src/features/checkout/Checkout.js
@@ -1,17 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../navigation/Navbar";
 import { useSelector } from "react-redux";
 import '../../styles/checkout.css'
+import { Button, Step, StepButton, Stepper } from "@mui/material";
 
 function Checkout(){
+    const [activeStep, setActiveStep] = useState(0)
+    let nextBtnDisplay = {display:'inline'}
+    let placeOrderBtn = {display:'none'}
+    let backBtnDisplay = {display:'inline'}
     const total = useSelector(state => state.total.value)
-    const steps = []
+    const steps = ['Billing Information', 'Shipping Information', 'Confirmation']
+
+    if (activeStep === steps.length -1){
+        nextBtnDisplay = {display : 'none'}
+        placeOrderBtn = {display : 'inline'}
+    } else if (activeStep === 0){
+        backBtnDisplay = {display:'none'}
+    }
+
+    function handleNext() {
+        if(activeStep + 1 < steps.length -1){
+            setActiveStep(activeStep + 1)
+        } else if (activeStep + 1 >= steps.length -1){
+            setActiveStep(steps.length - 1)
+        }
+    }
+
+    function handleBack() {
+        if(activeStep - 1 >= 0){
+            setActiveStep(activeStep - 1)
+        } else if (activeStep -1 < 0){
+            setActiveStep(0)
+        }
+    }
+
+    function handlePlaceOrder() {}
     return (
         <div className="main">
             <Navbar/>
             <div id="checkoutFormContainer">
-
+                <Stepper nonLinear activeStep={activeStep}>
+                    {steps.map((label, index) => (
+                        <Step key={label}>
+                            <StepButton onClick={() => setActiveStep(index)}>
+                                {label}
+                            </StepButton>
+                        </Step>
+                    ))}
+                </Stepper>
             </div>
+            <Button id="next" onClick={() => handleNext()} sx={nextBtnDisplay}>
+                Next
+            </Button>
+            <Button id="placeOrder" onClick={() => handlePlaceOrder()} sx={placeOrderBtn}>
+                Place Order
+            </Button>
+            <Button id="back" onClick={() => handleBack()} sx={backBtnDisplay}>
+                Back
+            </Button>
         </div>
     )
 }
