50fcd70e9343c4eecc52a1610d8abfc5687c23f5
nicopasq <90681300+nicopasq@users.noreply.github.com>
2024-01-01 19:13:12 -0700

    Added updatedBilling and updatedShipping objects to give values to form inputs.

--- client/src/features/profile/UpdateOrderModal.js
+++ client/src/features/profile/UpdateOrderModal.js
@@ -1,192 +1,242 @@
-import { Box, Button, Divider, Input, Modal, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  Input,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
-function UpdateOrderModal({displayUpdateForm, setDisplayUpdateForm, currentOrder}){
-    const style = {
-        borderRadius:'10px',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 700,
-        height:750,
-        bgcolor: 'background.paper',
-        boxShadow: 24,
-        p: 4,
-      };
+function UpdateOrderModal({
+  displayUpdateForm,
+  setDisplayUpdateForm,
+  currentOrder,
+}) {
+    const updatedBilling = {
+        holder_first_name: 'update firstName',
+        holder_last_name: 'updated lastName',
+        card_number: '9485857373648586',
+        expiration: '02-14-3041',
+        cvv: 648,
+    }
+    const updateShipping = {
+        first_name: 'updated first_name',
+        last_name: 'upddated last_name',
+        state: 'Dinosaur',
+        city: 'Colorado',
+        street_address: '1234 5th Street',
+        apt_number: 564,
+        zip_code: 40995
+    }
+  const style = {
+    borderRadius: "10px",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 700,
+    height: 750,
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 4,
+  };
 
-return (
+  function handleUpdateBilling(e){
+    e.preventDefault()
+    console.log(updatedBilling)
+  }
+
+  function handleUpdateShipping(e){
+    e.preventDefault()
+    console.log(updateShipping)
+  }
+
+  return (
     <Modal
-    open={displayUpdateForm}
-    onClose={() => setDisplayUpdateForm(false)}
-    aria-labelledby="modal-modal-title"
-    aria-describedby="modal-modal-description"
-  >
-    <Box sx={style}>
+      open={displayUpdateForm}
+      onClose={() => setDisplayUpdateForm(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
         <Typography variant="h5">Billing</Typography>
-        <form className="orderForm" id="billingForm" style={{marginLeft:'20%', marginBottom:'2%'}}>
-      <label>
-        <Typography variant="h6" sx={{ fontFamily: "serif" }}>
-          <u>Name on Card:</u>
+        <form
+          id='updateBillingInfo'
+          className="orderForm"
+          onSubmit={(e) => handleUpdateBilling(e)}
+          style={{ marginLeft: "20%", marginBottom: "2%" }}
+        >
+          <label>
+            <Typography variant="h6" sx={{ fontFamily: "serif" }}>
+              <u>Name on Card:</u>
+            </Typography>
+          <TextField
+            label="First Name"
+            sx={{ margin: "5px" }}
+            type="text"
+            value={updatedBilling.holder_first_name}
+            />
+          <TextField
+            label="Last Name"
+            sx={{ margin: "5px" }}
+            type="text"
+            value={updatedBilling.holder_last_name}
+            />
+            </label>
+
+          <div className="orderInput">
+            <label>
+              <Typography variant="subtitle1">
+                <u>Card Number</u>
+              </Typography>
+            <Input
+              id ='card_number'
+              name="card_number"
+              className="formInput"
+              value={updatedBilling.card_number}
+              />
+              </label>
+          </div>
+
+          <div className="orderInput">
+            <label>
+              <Typography variant="subtitle1">
+                <u>Expiration Date</u>
+              </Typography>
+            <Input id="expiration" className="formInput" value={updatedBilling.expiration} />
+            </label>
+          </div>
+
+          <div className="orderInput">
+            <label>
+              <Typography variant="subtitle1">
+                <u>Security Code</u>
+              </Typography>
+            <Input id="securityCode" className="formInput" type="number" value={updatedBilling.cvv} />
+            </label>
+          </div>
+
+          <Button
+            sx={{ width: "30%", marginLeft: "5%", marginBottom: "5%" }}
+            type="submit"
+          >
+            Submit Billing Information
+          </Button>
+        </form>
+
+        <Divider sx={{ bgcolor: "lightBlue" }} />
+
+        <Typography variant="h5" marginTop={"1%"} >
+          Shipping
         </Typography>
-      </label>
-      <TextField
-        label="First Name"
-        sx={{ margin: "5px" }}
-        type="text"
-        value="Updated First Name"
-      />
-      <TextField
-        label="Last Name"
-        sx={{ margin: "5px" }}
-        type="text"
-        value="Updated Last Name"
-      />
-      <div className="orderInput">
-        <label>
-          <Typography variant="subtitle1">
-            <u>Card Number</u>
+        <form
+          onSubmit={(e) => handleUpdateShipping(e)}
+          className="orderForm"
+          id="updateShippingForm"
+          style={{ marginLeft: "20%" }}
+        >
+          <Typography variant="h6">
+            <u>Recipient:</u>
           </Typography>
-        </label>
-        <Input
-        name="card_number" 
-        className="formInput" 
-        value='9876987698769876'
-        />
-      </div>
+          <div className="orderInput">
+            <label>
+              <Typography variant="subtitle1">
+                <u>First Name</u>
+              </Typography>
+            <TextField
+              label="First Name"
+              sx={{ margin: "5px" }}
+              type="text"
+              value={updateShipping.first_name}
+              />
+              </label>
+          </div>
 
-      <div className="orderInput">
-        <label>
-          <Typography variant="subtitle1">
-            <u>Expiration Date</u>
-          </Typography>
-        </label>
-        <Input
-        className="formInput"  
-        value='03-14-3041'
-         />
-      </div>
+          <div className="orderInput">
+            <label>
+              <Typography variant="subtitle1">
+                <u>Last Name</u>
+              </Typography>
+            <TextField
+              label="Last Name"
+              sx={{ margin: "5px" }}
+              type="text"
+              value={updateShipping.last_name}
+              />
+              </label>
+          </div>
 
-      <div className="orderInput">
-        <label>
-          <Typography variant="subtitle1">
-            <u>Security Code</u>
-          </Typography>
-        </label>
-        <Input 
-        className="formInput" 
-        type="number"
-        value={789}
-        />
-      </div>
-      <Button sx={{width:'30%', marginLeft:'5%', marginBottom:'5%'}} type="submit">Submit Billing Information</Button>
-    </form>
+          <div className="orderInput">
+            <label>
+              <Typography variant="subtitle1">
+                <u>State</u>
+              </Typography>
+            <TextField
+              label="State"
+              sx={{ margin: "5px" }}
+              type="text"
+              value={updateShipping.state}
+              />
+              </label>
+          </div>
 
-    <Divider sx={{bgcolor:'lightBlue'}}/>
+          <div className="orderInput">
+            <label>
+              <Typography variant="subtitle1">
+                <u>City</u>
+              </Typography>
+            <TextField
+              label="City"
+              sx={{ margin: "5px" }}
+              type="text"
+              value={updateShipping.city}
+              />
+              </label>
+          </div>
 
-    <Typography variant="h5" marginTop={'2%'} marginBottom={'5%'}>Shipping</Typography>
-        <form className="orderForm" id="shippingForm" style={{marginLeft:'20%'}}> 
-      <Typography variant="h6"><u>Recipient:</u></Typography>
-      <div className="orderInput">
-        <label>
-          <Typography variant="subtitle1">
-            <u>First Name</u>
-          </Typography>
-        </label>
-        <TextField
-          label="First Name"
-          sx={{ margin: "5px" }}
-          type="text"
-          value={'updated first_Name'}
-        />
-      </div>
+          <div className="orderInput">
+            <label>
+              <Typography variant="subtitle1">
+                <u>Street Address</u>
+              </Typography>
+            <Input
+              id = "street_address"
+              className="formInput"
+              type="text"
+              value={updateShipping.street_address}
+              />
+              </label>
+          </div>
 
-      <div className="orderInput">
-        <label>
-          <Typography variant="subtitle1">
-            <u>Last Name</u>
-          </Typography>
-        </label>
-        <TextField
-          label="Last Name"
-          sx={{ margin: "5px" }}
-          type="text"
-          value={"updated last_Name"}
-        />
-      </div>
+          <div className="orderInput">
+            <label>
+              <Typography variant="subtitle1">
+                <u>Apt. #</u>
+              </Typography>
+            <Input id="apt_number" className="formInput" type="number" value={updateShipping.apt_number} />
+            </label>
+          </div>
 
-      <div className="orderInput">
-        <label>
-          <Typography variant="subtitle1">
-            <u>State</u>
-          </Typography>
-        </label>
-        <TextField
-          label="State"
-          sx={{ margin: "5px" }}
-          type="text"
-          value={"Colorado"}
-        />
-      </div>
-
-      <div className="orderInput">
-        <label>
-          <Typography variant="subtitle1">
-            <u>City</u>
-          </Typography>
-        </label>
-        <TextField
-          label="City"
-          sx={{ margin: "5px" }}
-          type="text"
-          value={'Dinosaur'}
-        />
-      </div>
-
-      <div className="orderInput">
-        <label>
-          <Typography variant="subtitle1">
-            <u>Street Address</u>
-          </Typography>
-        </label>
-        <Input
-          className="formInput"
-          type="text"
-          value={'1234 5th street'}
-        />
-      </div>
-
-      <div className="orderInput">
-        <label>
-          <Typography variant="subtitle1">
-            <u>Apt. #</u>
-          </Typography>
-        </label>
-        <Input
-          className="formInput"
-          type="number"
-          value={546}
-        />
-      </div>
-
-      <div className="orderInput">
-        <label>
-          <Typography variant="subtitle1">
-            <u>Zip Code</u>
-          </Typography>
-        </label>
-        <Input
-          className="formInput"
-          type="number"
-          value={80422}
-        />
-      </div>
-      <Button sx={{width:'30%', marginLeft:'5%', marginBottom:'5%'}} type="submit">Submit Shipping Information</Button>
-    </form>
-    </Box>
-  </Modal>
-)
+          <div className="orderInput">
+            <label>
+              <Typography variant="subtitle1">
+                <u>Zip Code</u>
+              </Typography>
+            <Input id="zip_code" className="formInput" type="number" value={updateShipping.zip_code} />
+            </label>
+          </div>
+          <Button
+            sx={{ width: "30%", marginLeft: "5%", marginBottom: "5%" }}
+            type="submit"
+          >
+            Submit Shipping Information
+          </Button>
+        </form>
+      </Box>
+    </Modal>
+  );
 }
 
-export default UpdateOrderModal
-
+export default UpdateOrderModal;
