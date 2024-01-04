63839fdb48eed9a0922a9c16604f51fa1d5e07af
nicopasq <90681300+nicopasq@users.noreply.github.com>
2024-01-03 20:22:33 -0700

    save

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,7 +8,7 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 # builds the back end code
-bundle exec rake db:reset
 bundle install
+bundle exec rake db:reset
 bundle exec rake db:migrate
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
