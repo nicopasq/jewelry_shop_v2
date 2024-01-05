82a5b942dff1bdb2483c77dff29130ea17a8c366
nicopasq <90681300+nicopasq@users.noreply.github.com>
2024-01-04 22:40:16 -0700

    improved order_products_controller.

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -1,54 +1,68 @@
 class OrderProductsController < ApplicationController
-rescue_from ActiveRecord::RecordInvalid, with: :invalid_order_product
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_order_product
+    before_action :authenticate_user
     wrap_parameters format: []
 
     def create
-        user = User.find_by(id:session[:user_id])
-        # byebug
         if params[:ring]
-            order_product = user.order_products.find_by(product_id:params[:product_id], size:params[:size], in_cart:true)
-            if order_product && params[:quantity] > 0
-                order_product.update(quantity:order_product[:quantity] + params[:quantity])
-                render json: order_product, status: :accepted
-            elsif !order_product 
-                user.order_products.create!(orderProductParams)
-                order_product = user.order_products.last
-                render json: order_product, status: :created
-            end
+            update_order_product_with_ring || create_new_order_product_with_ring
         else
-            order_product = user.order_products.find_by(product_id:params[:product_id], in_cart:true)
-            if order_product 
-                order_product.update(quantity:order_product[:quantity] + params[:quantity])
-                render json: order_product, status: :accepted
-            elsif !order_product 
-                user.order_products.create!(orderProductParams)
-                order_product = user.order_products.last
-                render json: order_product, status: :created
-            end
+            update_order_product || create_new_order_product
         end
-
     end
 
     def destroy
-        user = User.find_by(id:session[:user_id])
-        user.order_products.destroy(params[:id])
+        current_user.order_products.destroy(params[:id])
         head:no_content
     end
 
     def update
-        user = User.find_by(id: session[:user_id])
-        order_product = user.order_products.find_by(id: params[:id])
+        order_product = current_user.order_products.find_by(id: params[:id])
         order_product.update(order_id:params[:order_id], in_cart:false)
         render json: order_product
     end
 
     private
 
-    def orderProductParams
-        params.permit(:user_id, :product_id, :size, :quantity, :in_cart, :order_id, :ring)
+    def authenticate_user
+        @current_user ||= User.find_by(id: session[:user_id])
     end
 
-    def invalid_order_product invalid
-        render json: {errors: invalid.record.errors.full_messages}, status: 422
+    def current_user
+        @current_user
+    end
+
+    def order_product_params
+        params.permit(:product_id, :size, :quantity, :in_cart, :order_id)
+    end
+
+    def update_order_product_with_ring
+        current_user.order_products.find_by(product_id:params[:product_id], size:params[:size], in_cart:true).try do |order_product|
+            order_product.update(quantity:order_product[:quantity] + params[:quantity])
+            render json: order_product, status: :accepted
+        end
+    end
+
+    def create_new_order_product_with_ring
+        current_user.order_products.create!(order_product_params)
+        order_product = current_user.order_products.last
+        render json: order_product, status: :created
+    end
+
+    def update_order_product
+        current_user.order_products.find_by(product_id:params[:product_id], in_cart:true).try do |order_product|
+            order_product.update(quantity:order_product[:quantity] + params[:quantity])
+            render json: order_product, status: :accepted
+        end
+    end
+
+    def create_new_order_product
+        current_user.order_products.create!(order_product_params)
+        order_product = current_user.order_products.last
+        render json: order_product, status: :created
+    end
+
+    def invalid_order_product(exception)
+        render json: { errors: exception.record.errors.full_message }, status: :unprocessable_entity
     end
 end
