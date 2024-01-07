98f70109ef8165b9ea8658066121b60bf6ba405d
Nico <nicopasquariello@gmail.com>
2024-01-07 13:16:17 -0700

    Validations no longer inhibit Order_Product updates when creating an Order.

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -6,6 +6,6 @@ class OrderProduct < ApplicationRecord
     validates :product_id, presence:true
     validates :user_id, presence:true
     validates :quantity, presence:true, numericality:{greater_than: 0}
-    validates :ring, presence: true
-    validates :in_cart, presence: true
+    validates :ring, presence:true, allow_blank: true
+    validates :in_cart, presence:true, on: :create
 end
