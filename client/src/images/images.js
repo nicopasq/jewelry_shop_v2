5d160ecfaf86fa80ca18bbcd964aea015968d023
Nico <nicopasquariello@gmail.com>
2024-07-16 14:08:36 -0600

    init

--- client/src/images/images.js
+++
@@ -1,9 +0,0 @@
-import ring_images from './rings/rings.js'
-import necklace_images from './necklaces/necklaces.js';
-import earring_images from './earrings/earrings.js'
-
-let product_images = []
-
-product_images = product_images.concat(ring_images, necklace_images, earring_images)
-
-export default product_images;
